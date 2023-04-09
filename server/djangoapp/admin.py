b78bd0638bfe9e8e2d0374b7fd94bcdae4939b54
Joseph Adogeri <jadogeri@uno.edu>
2023-04-09 14:55:22 -0500

    added carmodel  and carmake to admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -11,3 +11,27 @@ from django.contrib import admin
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+from django.contrib import admin
+
+
+from django.contrib import admin
+from .models import CarMake, CarModel
+
+
+# Register your models here.
+admin.site.register(CarMake)
+
+admin.site.register(CarModel)
+
+
+# CarModelInline class
+
+class CarModelInline:
+    pass
+
+
+# CarModelAdmin class
+
+# CarMakeAdmin class with CarModelInline
+
+# Register models here
